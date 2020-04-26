baa09652f9184952b94de6eaba5bc1f95af1e65e
NAKAGAKI Kenji <nakaken0629@gmail.com>
2020-04-26 19:19:36 +0900

    c2-1を作成

---
+++ c2/c2-1.c
@@ -0,0 +1,89 @@
+/* code: c2-1.c (v1.18.0) */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 128
+#define PUSH_SUCCESS 1
+#define PUSH_FAILUER -1
+#define POP_SUCCESS 2
+#define POP_FAILUER -2
+
+int peek (int stack[], int *top, int *data) {
+    if (*top > 0) {
+        *data = stack[(*top) - 1];
+        return POP_SUCCESS;
+    } else {
+        /* stack_empty */
+        return POP_FAILUER;
+    }
+}
+
+void stack_init (int *top) {
+    *top = 0;
+}
+
+void display (int stack[], int top) {
+    int i;
+    printf ("STACX (%d) : ", top);
+    for (i = 0; i < top; i++) {
+        printf ("%d ", stack[i]);
+    }
+    printf ("\n");
+}
+
+int push (int stack[], int *top, int data) {
+    if (*top >= MAX) {
+        /* stack overflow */
+        return PUSH_FAILUER;
+    } else {
+        stack[*top] = data;
+        (*top)++;
+        return PUSH_SUCCESS;
+    }
+}
+
+int pop (int stack[], int *top, int *data) {
+    if ((*top) > 0) {
+        *data = stack[ (*top) - 1];
+        (*top)--;
+        return POP_SUCCESS;
+    } else {
+        /* stack empty */
+        return POP_FAILUER;
+    }
+}
+
+int main() {
+    int stack[MAX];
+    int top, data;
+
+    stack_init(&top);
+    data = 300;
+    printf ("push: %d\n", data);
+    push (stack, &top, data);
+    data = 400;
+    printf ("push: %d\n", data);
+    push (stack, &top, data);
+    data = 500;
+    printf ("push: %d\n", data);
+    push (stack, &top, data);
+
+    peek(stack, &top, &data);
+    printf ("peek: %d\n", data);
+    peek(stack, &top, &data);
+    printf ("peek: %d\n", data);
+
+    display(stack, top);
+
+    pop (stack, &top, &data);
+    printf ("pop: %d\n", data);
+
+    peek(stack, &top, &data);
+    printf ("peek: %d\n", data);
+
+    pop (stack, &top, &data);
+    printf ("pop: %d\n", data);
+    pop (stack, &top, &data);
+    printf ("pop: %d\n", data);
+    return 0;
+}
